61389befc6f0766e82844cdc6a9ffc035349ebd0
Elishafan <elishafan.machado@gmail.com>
2020-08-09 15:13:40 -0300

    Criação do banco de dados e da operação Post

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 require('./models/Dados')
 const Dados = mongoose.model('dados');
 
+const app = express();
+
+app.use(express.json());
+
 mongoose.connect('mongodb://localhost/api', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -13,13 +17,24 @@ mongoose.connect('mongodb://localhost/api', {
     console.log("Erro: Conexão com MongoDB não foi realizada com sucesso!");
 })
 
-const app = express();
-
-app.use(express.json());
 
 app.get("/", (req, res) => {
     return res.json({ titulo: "Inicializando a API"})
 })
+
+app.post("/dados", (req, res) => {
+    const dados = Dados.create(req.body, (err) => {
+        if(err) return res.status(400).json({
+            erro: true,
+            message: "Error: Artigo não foi cadastrado com sucesso!"
+        });
+        return res.status(200).json({
+            error: false,
+            message: "Artigo cadastrado com sucesso!"
+        })
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("Servidor iniciado na porta 3000: http://localhost:3000/");
